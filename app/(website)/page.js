a9397c374fc54da8964af13d18adbe3ffcf08f1c
Sonika Sharma <sonikasharma@Sonikas-MacBook-Air.local>
2024-04-27 00:51:29 +0530

    fix(main): enables revalidate

--- app/(website)/page.js
+++ app/(website)/page.js
@@ -1,10 +1,6 @@
 import HomePage from "./home";
 import { getAllPosts } from "@/lib/sanity/client";
 
-export const dynamic = "force-dynamic";
-
-export const runtime = "edge";
-
 export default async function IndexPage() {
   const posts = await getAllPosts();
   return <HomePage posts={posts} />;
